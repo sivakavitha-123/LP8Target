c296dac0a4289b6bc9447ca87b3411fc49bef5f3
sivakavitha-123 <sivakavitha1999@gmail.com>
2026-06-10 22:54:27 +0530

    Implement number swapping in Swappingtwonumbers.java

---
+++ Swappingtwonumbers.java
@@ -0,0 +1,15 @@
+public classname Swapping()
+  {
+  public static void main(Strings[] args)
+    {
+    int a = 10;
+    int b= 20;
+    int temp ;
+
+    temp=a;
+    a=b;
+    b=temp;
+    System.out.println(a);
+    System.out.println(b);
+  }
+}
