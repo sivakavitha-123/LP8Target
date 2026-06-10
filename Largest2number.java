23941022400776c13605e25025ed30717035255d
sivakavitha-123 <sivakavitha1999@gmail.com>
2026-06-10 23:13:18 +0530

    Implement logic to find the largest of two numbers
    Fix syntax errors and improve logic for finding the largest number.

---
+++ Largest2number.java
@@ -0,0 +1,29 @@
+public class Largestnumber()
+  {
+  public Static void main(Strings[] args)
+    {
+
+    /*************using if else method ************/
+    int a = 70;
+    int b = 100;
+    if(a>b){
+      System.out.println("Largest number is "+ a);
+    }
+    else
+    {
+    System.out.println("Largest number is "+ b);
+    }
+/*************using Terninary method ************/
+
+boolean res = (a>b)? a:b
+if(res==true)
+  {
+  System.out.println("a is a Largest number");
+}
+else
+    {
+      System.out.println("b is a largest number");
+    }
+    
+  }
+}
