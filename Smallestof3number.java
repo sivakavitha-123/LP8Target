3805c6d833cb2316744e01ca0c510ef79c7b8d6a
sivakavitha-123 <sivakavitha1999@gmail.com>
2026-06-11 10:52:46 +0530

    Implement Smallest of 3 numbers logic in Java

---
+++ Smallestof3number.java
@@ -0,0 +1,22 @@
+public class Smallest3()
+  {
+  public static void main(strings[] args)
+    {
+    int a =40;
+    int b =10;
+    int c=31;
+    if (a<b)
+    {
+      System.out.println("a is smallest number":+ a);
+    }
+    else if(b<c)
+    {
+        System.out.println("b is smallest number":+ b);
+    }
+    else
+    {
+        System.out.println("c is smallest number":+ c);
+    }
+  }
+  
+      
